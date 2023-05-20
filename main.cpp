07c9b49e03baf9dd25ac7033f1bc5f79deb2cf28
mortalsniper12 <zaidimojiz4@gmail.com>
2023-05-20 12:26:20 +0500

    Update main.cpp
    added libraries

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>      
+#include <windows.h>          //for new interface
+#include <conio.h>           //for maths functions
+#include <stdlib.h>          //to make input output easy 
+#include <string.h>           
+#include <time.h>            //for printing current time and date
+#include <fstream>           //file handling
+#include <cstdio>            
+
+
+// scanf(%d, &int)  is being used to store the value in specified memory location.  
+/// can be replaced with cin if complex
