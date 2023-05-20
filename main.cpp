d1577e6512da5dac853b2720b9ba7808d105eed0
mortalsniper12 <u2022520@giki.edu.pk>
2023-05-20 19:49:00 +0500

    Update main.cpp
    ADED FIRST CLASS INITIALIZED FUNCTIONS

--- main.cpp
+++ main.cpp
@@ -10,3 +10,366 @@
 
 // scanf(%d, &int)  is being used to store the value in specified memory location.  
 /// can be replaced with cin if complex
+
+
+
+using namespace std;
+
+//global initializing integers
+
+int x; 
+
+int ca(int a);                                      //function for checking accounts
+
+class gikipay{
+	
+   void check_balance();
+   void withdrawl();
+   void deposit();
+   void transfer();
+   void mainexit();                       // initializing member functions to the gikipay class
+   void view();                              
+   void add();
+   void del();
+   void edit();
+   void search_name();
+   void search_acn();                   
+  
+   
+   
+
+//continuous code starts from here.
+
+
+//data encaptulation
+	 
+protected: 
+          // data hiding
+      char id[20],password[15];          //used for logging in the main system 
+      
+      
+   private:
+    int choice;                            // choice for admin or user
+    
+    
+   public: // data abstraction
+   
+       gikipay(){                                      //constructor.
+	     
+		 cout << "\t\t\t    //////////////////////////////////" << endl;
+         cout << "\t\t\t   //------------------------------//" << endl;
+         cout << "\t\t\t  ///|        GIKI PAY          |///" << endl;
+         cout << "\t\t\t ///----------------------------///" << endl;
+         cout << "\t\t\t//////////////////////////////////" << endl;
+         cout << "\t\t\t-----------------------------------" << endl;
+         cout << "\t\t\t| AAPKAY DIL MAIN HAMARA ACCOUNT |" << endl;
+         cout << "\t\t\t-----------------------------------" << endl;                                     
+       
+	   
+	     cout << "CHOOSE ACCOUNT TYPE: ";
+         cout << endl;
+         
+         cout << "[1] . ADMINISTRATOR ";
+         cout << "\t\t\t" ;
+         cout << "[2] . USER ";
+         cout << endl;
+         cout << "Enter Your Choice .... ";
+         cin >> choice;
+         
+         if (choice != 1 && choice != 2) {
+         	system("cls");// to a new screen  
+            gikipay();
+         }
+         system("cls");
+         authentication();
+	   }
+	   
+	 	
+	   
+	   void authentication() {
+
+      cout << "Welcome to GIKI Pay Banking System - where your finances are in safe hands! "<<endl;
+
+      login();
+      cout<<endl<<endl<<endl;
+      if (verify() == 1) {
+         if (choice== 1)
+            main();                  //for admin
+         else
+            transaction();             // for user
+      } 
+	  else if (verify() == 0) {
+         system("cls");
+         cout << "Incorrect Username / Password !!!!";
+         cout << endl;
+         authentication();
+      }
+   }
+	 
+	 
+	 
+	 void login(){                                    // Takes the user-name and password .
+
+      cout << "Enter The Username : ";
+      cin >> id;                                          //initialized globally in gikipay function.
+
+      cout << "Enter The Password : ";
+      cin >> password;                                 //initialized globally in gikipay function.
+   }
+
+   
+
+	 // verifies the user-name and password .
+	 int verify(){ 
+      char a;
+      if (choice == 1) {                       //if choice is admin 
+         
+		 if ((strcmp(id, "admin") == 0 && strcmp(password, "admin") == 0)) {
+
+            cout << "You Have Successfully Logged In : " << '"' << id << '"' << endl;
+            time_t t;
+            time( & t);
+
+            cout << "Logged In Time : " << ctime( & t);
+
+            cout << "Press any key to continue .... ";
+            getch();          
+            return 1;
+         } else
+            return 0;
+      } else if (choice == 2) {
+         if (strcmp(id, "user") == 0 && strcmp(password, "user") == 0) {
+
+            cout << "You Have Successfully Logged In : " << '"' << id << '"' << endl;
+            time_t t;
+            time( & t);
+
+            cout << "Logged In Time : " << ctime( & t);
+
+            cout << "Press any key to continue .... ";
+            getch();
+            return 1;
+         } else
+            return 0;
+      }
+      return 0;
+   }
+
+
+
+
+
+  void main() {                                            // displays the main user interface screen of program .
+     system("cls");
+
+      cout << " WELCOME TO MAIN MENU ";
+      cout << endl;
+
+      cout << "[1] . View Customer Accounts";
+      cout << endl;
+
+      cout << "[2] . Customer Account Registration";
+      cout << endl;
+
+      cout << "[3] . Edit Customer Account";
+      cout << endl;
+
+      cout << "[4] . Delete Customer Account";
+      cout << endl;
+
+      cout << "[5] . Search Customer Account";
+      cout << endl;
+
+      cout << "[6] . Transaction";
+      cout << endl;
+
+      cout << "[7] . Log Out !!! ";
+      cout << endl;
+
+      cout << "[8] . About US ";
+      cout << endl;
+
+      cout << "Please Enter Your Choice [1-8] : ";
+      mainchoice();
+   }
+   
+   
+   
+   
+   void mainchoice() {                                        // takes user choice in main  and goes to desired function .
+   
+      int i;
+      cin >> i;
+      cout<<endl;
+      system("cls");
+      
+      switch (i) {
+      case 1:
+         view();
+         break;
+      case 2:
+         add();
+         break;
+      case 3:
+         edit();
+         break;
+      case 4:
+         del();
+         break;
+      case 5:
+         search();
+         break;
+      case 6:
+         transaction();
+         break;
+      case 7:
+         mainexit();
+         break;
+      case 8:
+         about();
+         break;
+      default:
+         main();
+      }
+   }
+
+
+	
+	
+	
+	
+ void search() {                               //module for searching account ( 2 types by number and name )
+ 
+ system("cls");
+      int search_choice;
+
+      cout << " SEARCH MENU: ";
+      cout << endl;
+
+      cout << "[1] . Search By Account ";
+      cout << endl;
+
+      cout << "[2] . Search By Name ";
+      cout << endl;
+
+      cout << "Enter Your Choice [1-2] : ";
+      cin>>search_choice;
+      system("cls");
+      
+      if (search_choice== 1) {
+         search_acn();
+      }
+	   else if (search_choice== 2) {
+         search_name();
+      } else
+         main();
+   }
+  
+	
+	
+	
+	
+	
+	
+	
+	void transaction(){                        // displays screen for the transaction options and takes the user choice .
+	
+	system("cls");
+	
+	  cout << " TRANSACTION MENU ";
+      cout << endl;
+
+      cout << "[1] . Balance Inquiry";
+      cout << endl;
+
+      cout << "[2] . Cash Deposit";
+      cout << endl;
+
+      cout << "[3] . Cash Withdrawal";
+      cout << endl;
+
+      cout << "[4] . Fund Transfer";
+      cout << endl;
+      
+      
+      if (choice == 1) {
+           cout << "[5] . Main Menu";
+      } 
+	  else {
+         cout << "[5] . Exit";
+      }
+		
+		cout<<endl<<endl<<endl;
+		
+	  cout << "Please Enter Your Choice [1-5] : ";
+      int transaction_choice;
+      cin>> transaction_choice;
+      
+      switch (transaction_choice) {
+      case 1:
+         check_balance();
+         break;
+      case 2:
+         deposit();
+         break;
+      case 3:
+         withdrawl();
+         break;
+      case 4:
+         transfer();
+         break;
+      case 5:
+         if (transaction_choice == 1)
+            main();
+         else
+            mainexit();
+         break;
+      default:
+         transaction();
+      }
+   }
+     
+   
+   
+   
+   
+   
+   
+   void about(){
+   	
+   	  cout << " ABOUT US ";
+	  cout<<endl;
+	  
+      cout << "It is a  Bank Management System Project for our 2nd Semester based on C++ programming language.";
+      cout<<endl;
+      	
+      cout << "Members of Our Team: ";
+      cout<<endl;
+      	
+      	
+      	
+      cout << "[1] . Saba Hareem Shaikh";
+      cout<<endl;
+      	
+      cout << "[2] . Syed Mojiz Zaidi";
+      cout<<endl;
+      	
+      cout<<"[3] .  Dua Tahir";
+      cout<<endl;
+      
+      cout<<"[4] . Muhammad Abdullah";
+      cout<<endl;
+      
+      
+      cout << "Press any key to return back to main menu. ";
+      char z = getch();
+      
+      if (z == 13) {
+         main();
+      }
+   }
+	
+};               // class gikipay ends here. code of project mile stone 1;  file handling will be done in later classes.
+
+
+
